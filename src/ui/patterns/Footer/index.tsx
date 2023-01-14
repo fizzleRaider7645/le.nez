ccab92a5d70c008fee13d34e5807cb4c9b27924b
Douglas Uretsky <douglasuretsky@gmail.com>
2023-01-14 13:27:21 -0500

    update footer

--- src/ui/patterns/Footer/index.tsx
+++ src/ui/patterns/Footer/index.tsx
@@ -1,9 +1,15 @@
+import { useIntl } from "react-intl";
+
 function Footer() {
+  const { formatMessage } = useIntl();
+  const copyrightLabel =
+    formatMessage({ id: "copyright" }).charAt(0).toUpperCase() +
+    formatMessage({ id: "copyright" }).slice(1);
   return (
     <footer className='bg-gray-200 py-8'>
       <div className='container mx-auto flex items-center justify-between flex-wrap'>
         <div className='w-1/2 text-center text-gray-700 text-sm'>
-          Copyright 2023 &copy; le.nez
+          {copyrightLabel} 2023 &copy; le.nez
         </div>
       </div>
     </footer>
