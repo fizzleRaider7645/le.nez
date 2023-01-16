75bb09bc368f60948458232094826c17f2517060
Douglas Uretsky <douglasuretsky@gmail.com>
2023-01-15 19:00:46 -0500

    WIP

--- src/ui/patterns/Form/index.tsx
+++ src/ui/patterns/Form/index.tsx
@@ -2,18 +2,41 @@ import { createContext, useReducer, useState } from "react";
 import DragAndDrop from "../DragAndDrop";
 import reducer, { initialState } from "./reducer";
 import { SelectionInputs, TextInputs } from "./subomponents";
-import { FormActionType, FormState } from "./types";
+import { FormActionType, FormArrowProps, FormState } from "./types";
 
 export const FormContext = createContext<{
   formState: FormState;
   dispatch: React.Dispatch<{
     type: FormActionType;
-    payload?: any;
+    payload: string;
   }>;
 }>({
   formState: initialState,
   dispatch: () => {},
 });
+
+const Arrow = ({ directionOverride, step, steps }: FormArrowProps) => {
+  const isLastStep = step === steps.length - 1;
+  const directions = { left: 270, right: 90 };
+  const directionToUse = directionOverride
+    ? directions[directionOverride]
+    : isLastStep
+    ? directions.left
+    : directions.right;
+
+  return (
+    <div>
+      <svg
+        width='24'
+        height='24'
+        viewBox='0 0 24 24'
+        transform={`rotate(${directionToUse})`}
+      >
+        <path d='M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z' />
+      </svg>
+    </div>
+  );
+};
 
 function Form() {
   const steps = [<SelectionInputs />, <TextInputs />, <DragAndDrop />];
@@ -22,16 +45,40 @@ function Form() {
 
   return (
     <FormContext.Provider value={{ formState, dispatch }}>
-      <form className='w-full max-w-md'>
-        {steps[step]}
-        <button
-          onClick={(event) => {
-            event.preventDefault();
-            setStep((step + 1) % steps.length);
-          }}
-        >
-          Click
-        </button>
+      <form className='w-full flex'>
+        <div className='flex-1'>{steps[step]}</div>
+        {step === 0 || step === steps.length - 1 ? (
+          <button
+            className='ml-5 w-15 rounded-full bg-transparent shadow-xl focus:outline-none'
+            onClick={(event) => {
+              event.preventDefault();
+              setStep((step + 1) % steps.length);
+            }}
+          >
+            <Arrow step={step} steps={steps} />
+          </button>
+        ) : (
+          <>
+            <button
+              className='ml-5 w-15 rounded-full bg-transparent shadow-xl focus:outline-none'
+              onClick={(event) => {
+                event.preventDefault();
+                setStep((step + 1) % steps.length);
+              }}
+            >
+              <Arrow directionOverride='left' step={step} steps={steps} />
+            </button>
+            <button
+              className='ml-5 w-15 rounded-full bg-transparent shadow-xl focus:outline-none'
+              onClick={(event) => {
+                event.preventDefault();
+                setStep((step + 1) % steps.length);
+              }}
+            >
+              <Arrow directionOverride='right' step={step} steps={steps} />
+            </button>
+          </>
+        )}
       </form>
     </FormContext.Provider>
   );
--- src/ui/patterns/Form/types.ts
+++ src/ui/patterns/Form/types.ts
@@ -13,3 +13,9 @@ export interface FormState {
 
 
 export type FormActionType = 'UPDATE_OCCASION' | 'UPDATE_DESIRED_EFFECT' | 'UPDATE_SCENT_TYPE' | 'UPDATE_SCENT_NOTES' | 'UPDATE_PERSONALITY' | 'UPDATE_PREVIOUS_EXPERIENCES' | 'UPDATE_DISLIKED_NOTES' | 'UPDATE_SKIN_TYPE' | 'UPDATE_CLIMATE' | 'UPDATE_SILLIAGE'
+
+export interface FormArrowProps {
+    directionOverride?: 'right' | 'left'
+    step: number;
+    steps: JSX.Element[];
+}
