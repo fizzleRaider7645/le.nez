00bde243b976caa23223b47964a6d7f1fb941193
Douglas Uretsky <douglasuretsky@gmail.com>
2023-01-23 14:26:34 -0500

    clean up styling

--- src/ui/patterns/Main/index.tsx
+++ src/ui/patterns/Main/index.tsx
@@ -3,19 +3,15 @@ import Form from "../Form";
 
 function Main() {
   return (
-    <section className='py-8 w-full bg-transparent'>
-      <div className='container mx-auto px-4'>
-        <div className='flex'>
-          <div className='w-3/4'>
-            <h1 className='text-3xl font-bold text-gray-800 mb-6 font-michroma'>
-              <FormattedMessage id='welcome' />
-            </h1>
-            <Form />
-            <p className='text-gray-600 mb-8 font-michroma'>
-              <FormattedMessage id='mainContent' />
-            </p>
-          </div>
-        </div>
+    <section className='flex'>
+      <div className='w-3/4'>
+        <h1 className='text-3xl font-bold text-gray-800 mb-6 font-michroma'>
+          <FormattedMessage id='welcome' />
+        </h1>
+        <Form />
+        <p className='text-gray-600 mb-8 font-michroma'>
+          <FormattedMessage id='mainContent' />
+        </p>
       </div>
     </section>
   );
